b7cd9e1c7b3e93e769dbbc728cf8de6e102ac884
VladiGeorgiev <58029946+VladiGeorgiev@users.noreply.github.com>
2022-02-06 20:47:41 +0200

    Add files via upload

--- src/assets/js/component.js
+++ src/assets/js/component.js
@@ -1,1 +1,14 @@
+import logo from '../../assets/images/logo.svg';
 
+function component() {
+    let m = document.getElementById('main_div');
+    let p = document.createElement('p');
+    let img = document.createElement('img');
+    m.append(p);
+    p.append(img);
+    img.src = logo;
+    img.alt = 'sample logo';
+    m.classList.add("img-class");
+}
+
+export default component;
