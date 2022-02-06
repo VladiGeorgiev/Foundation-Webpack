203a045f9cffc0f4fa0127190631af6d1b5de76b
VladiGeorgiev <58029946+VladiGeorgiev@users.noreply.github.com>
2022-02-06 20:44:53 +0200

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
+//import $ from 'jquery';
+//window.jQuery = $;
+import style from './scss/main.scss';
+import component from './assets/js/component';
+//import Foundation from 'foundation-sites';
 
+//import './lib/foundation-explicit-pieces';
+
+//$(document).foundation();
+
+function requireAll(r) {
+    r.keys().forEach(r);
+}
+
+requireAll(require.context('./assets/sprite-images/', true, /\.svg$/));
+
+component();
